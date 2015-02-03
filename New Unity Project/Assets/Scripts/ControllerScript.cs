fbcb207a6a70ded14aaa13f0a3bd693459e3ecce
Mclean Oshiokpekhai <mxo7542@rit.edu>
2015-02-03 17:56:17 -0500

    Controller Script added with keyboard controls. TowTruck is now fixed in it's angle

--- New Unity Project/Assets/Scripts/ControllerScript.cs
+++ New Unity Project/Assets/Scripts/ControllerScript.cs
@@ -3,13 +3,21 @@ using System.Collections;
 
 public class ControllerScript : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
 	
 	// Update is called once per frame
 	void Update () {
 
+        // accelerometer controls
+        // transform.Translate(Input.acceleration.x, Input.acceleration.y, 0);
+
+        // keyboard controls
+        if (Input.GetKey(KeyCode.UpArrow)) // up
+            transform.Translate(0, 10*Time.deltaTime, 0);
+        if (Input.GetKey(KeyCode.DownArrow)) // down
+            transform.Translate(0, -10*Time.deltaTime, 0);
+        if (Input.GetKey(KeyCode.LeftArrow)) // left
+            transform.Translate(-10*Time.deltaTime, 0, 0);
+        if (Input.GetKey(KeyCode.RightArrow)) // right
+            transform.Translate(10*Time.deltaTime, 0, 0);
 	}
 }
