14945429aac4333a965cb39e2e75225e84f53294
Mclean Oshiokpekhai <mxo7542@rit.edu>
2015-02-27 23:03:08 -0500

    Added new Camera Script. Currently works only one way, up.
    Added new accelerometer controls for Android phones.
    
    Rebuilt android apk.

---
+++ New Unity Project/Assets/Scripts/CameraScript.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraScript : MonoBehaviour {
+
+    public GameObject truck;       //reference of the player object
+    private int currentNumCars;      //number of cars attached to the tow truck
+    private int previousNumCars = 0;     //number of cars attached to the tow truck during the last update
+
+    private const int BASE_CAMERA_ORTHO_SIZE = 5;
+
+    // Update is called once per frame
+	void Update () {
+
+        currentNumCars = truck.GetComponent<ChainLinkScript>().cars.Count;
+
+        if (currentNumCars >= 3)
+        {
+            if (previousNumCars < currentNumCars) //if cars where added last time
+                camera.orthographicSize++;
+            if (previousNumCars > currentNumCars)
+                camera.orthographicSize--;
+        }
+
+        previousNumCars = currentNumCars;
+
+        currentNumCars = 0;
+       
+	}
+}
--- New Unity Project/Assets/Scripts/ControllerScript.cs
+++ New Unity Project/Assets/Scripts/ControllerScript.cs
@@ -6,32 +6,48 @@ public class ControllerScript : MonoBehaviour {
     public Sprite leftTruck;
     public Sprite rightTruck;
     public Sprite forwardTruck;
+
+    public int speed = 20;
 	
 	// Update is called once per frame
 	void Update () {
 
         // accelerometer controls
-        // transform.Translate(Input.acceleration.x, Input.acceleration.y, 0);
+        Vector3 dir = new Vector3(0, 0, 0);
+        dir.x = Input.acceleration.x;
+
+        if (dir.x > 0)
+            GetComponent<SpriteRenderer>().sprite = leftTruck;
+        if (dir.x < 0)
+            GetComponent<SpriteRenderer>().sprite = rightTruck;
+
+        if (dir.sqrMagnitude > 1)
+            dir.Normalize();
+
+        
+        dir *= Time.deltaTime;
+        
+
 
         // keyboard controls
         if (Input.GetKey(KeyCode.UpArrow)) // up
         {
             this.GetComponent<SpriteRenderer>().sprite = forwardTruck;
-            transform.Translate(0, 10 * Time.deltaTime, 0);
+            transform.Translate(0, speed * Time.deltaTime, 0);
         }
         if (Input.GetKey(KeyCode.DownArrow)) // down
         {
             this.GetComponent<SpriteRenderer>().sprite = forwardTruck;
-            transform.Translate(0, -10*Time.deltaTime, 0);
+            transform.Translate(0, -speed*Time.deltaTime, 0);
         }
         if (Input.GetKey(KeyCode.LeftArrow)) // left
         {
-            transform.Translate(-10 * Time.deltaTime, 0, 0);
+            transform.Translate(-speed * Time.deltaTime, 0, 0);
             this.GetComponent<SpriteRenderer>().sprite = leftTruck;
         }
         if (Input.GetKey(KeyCode.RightArrow)) // right
         {
-            transform.Translate(10 * Time.deltaTime, 0, 0);
+            transform.Translate(speed * Time.deltaTime, 0, 0);
             this.GetComponent<SpriteRenderer>().sprite = rightTruck;
         }
         
